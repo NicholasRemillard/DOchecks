d922e4f064a19e534b2bfa53fed9bfff11da3cc9
NicholasRemillard <35978032+NicholasRemillard@users.noreply.github.com>
2025-01-13 16:47:45 -0500

    Added shiny app to view combined_plot and locked down the y axes to allow for horizontal scrolling only

--- quality_control_script.R
+++ quality_control_script.R
@@ -1,7 +1,7 @@
 # Script for video annotation quality control
 # Created by Nick Remillard 1/9/2024
 
-# Script last updated: 1/9/2024
+# Script last updated: 1/13/2024
 
 # SEC 1: Load necessary packages ----
 rm(list = ls())
@@ -14,6 +14,7 @@ library(svDialogs)
 library(gridExtra)
 library(stringr)
 library(plotly)
+library(shiny)
 library(htmlwidgets)
 
 source("DOchecks_functions.R")
@@ -36,7 +37,6 @@ beh_of_interest <- c("Treadmill Running at slower pace than normal", "Treadmill 
 
 # SEC 3: Select video to assess ----
 video1_info <- select_video(folder_path)
-#raw_file_v1 <- video1_info$raw_file
 
 # If else loop ----
 compare_files_yesno <- dlg_message("Are you comparing two files?", type = c("yesno"))$res
@@ -44,7 +44,6 @@ compare_files_yesno <- dlg_message("Are you comparing two files?", type = c("yes
 if(compare_files_yesno == "yes"){ # If comparing two videos and doing quality control
   
   video2_info <- select_video(folder_path)
-  #raw_file_v2 <- video2_info$raw_file
   
   # Compare all behaviors and modifiers
     # Set up behavior data frames - v1
@@ -57,76 +56,80 @@ if(compare_files_yesno == "yes"){ # If comparing two videos and doing quality co
     # Calculate percent agreement for modifier (2 videos only)
     mod_agree <- calc_perc_agreement(video1_frames$mod_frame, video2_frames$mod_frame)
   
-    # Total plot comparison (2 videos only)
-    plot_beh <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
-                    tall_data1 = tall_beh_v1, tall_data2 = tall_beh_v2,
-                    agree_list = beh_agree)
-    # 
-    # plot_mod <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
-    #                             tall_data1 = tall_mod_v1, tall_data2 = tall_mod_v2,
-    #                             agree_list = beh_agree)
   
   # Compare only periods with behaviors of interest
-    # Filter agreement data frames for only periods containing behaviors of interest
-    # beh_int_frame1 <- tall_beh_v1
-    # beh_int_frame1$behavior <- ifelse(beh_int_frame1$behavior %in% beh_of_interest, beh_int_frame1$behavior, "Other")
-    # 
-    # beh_int_frame2 <- tall_beh_v2
-    # beh_int_frame2$behavior <- ifelse(beh_int_frame2$behavior %in% beh_of_interest, beh_int_frame2$behavior, "Other")
-    # 
     # Calculate percent agreement for behavior (2 videos only)
     beh_int_agree <- calc_perc_agreement(video1_frames$beh_int_frame, video2_frames$beh_int_frame)
-    
-    # # Make mod_int_frames - need to put "Other" for same rows as beh_int_frames
-    # mod_int_frame1 <- tall_mod_v1
-    # mod_int_frame2 <- tall_mod_v2
-    # 
-    # other_rows1 <- which(beh_int_frame1$behavior %in% "Other")
-    # other_rows2 <- which(beh_int_frame1$behavior %in% "Other")
-    # 
-    # for(i in other_rows1){mod_int_frame1$modifier[i] <- "Other"}
-    # for(i in other_rows2){mod_int_frame2$modifier[i] <- "Other"}
     
     # Calculate percent agreement for modifier (2 videos only)
     mod_int_agree <- calc_perc_agreement(video1_frames$mod_int_frame, video2_frames$mod_int_frame)
     
+  # Total plot comparison (2 videos only)
+  plot_beh <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
+                              tall_data1 = video1_frames$beh_frame, tall_data2 = video2_frames$beh_frame,
+                              agree_list = beh_agree)
     
-    plot_beh_int <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
-                    tall_data1 = beh_int_frame1, tall_data2 = beh_int_frame2)
+  plot_mod <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
+                              tall_data1 = video1_frames$mod_frame, tall_data2 = video2_frames$mod_frame,
+                              agree_list = beh_agree)
+    
+  plot_beh_int <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
+                  tall_data1 = video1_frames$beh_int_frame, tall_data2 = video2_frames$beh_int_frame,
+                  agree_list = beh_int_agree)
   
+  plot_mod_int <- plot_annotation(video1_info = video1_info, video2_info = video2_info,
+                                  tall_data1 = video1_frames$mod_int_frame, tall_data2 = video2_frames$mod_int_frame,
+                                  agree_list = mod_int_agree)
   
-  # Create your ggplotly objects
-  plot_beh_int_interactive <- ggplotly(plot_beh_int)
+  # Create ggplotly objects
   plot_beh_interactive <- ggplotly(plot_beh)
+  plot_beh_int_interactive <- ggplotly(plot_beh_int)
   
-  combined_plot <- subplot(plot_beh_int_interactive, plot_beh_interactive, nrows = 2)
+  plot_mod_interactive <- ggplotly(plot_mod)
+  plot_mod_int_interactive <- ggplotly(plot_mod_int)
   
-  # Save the combined plot as an HTML file
-  saveWidget(combined_plot, "combined_plots.html", selfcontained = TRUE)
-  
-  # Open the combined plot in the default browser
-  browseURL("combined_plots.html")
+  combined_plot <- subplot(plot_beh_int_interactive, plot_beh_interactive,
+                           plot_mod_int_interactive, plot_mod_interactive,
+                           nrows = 4, shareX = TRUE) %>%
+    layout(
+      yaxis = list(fixedrange = TRUE),
+      yaxis2 = list(fixedrange = TRUE),
+      yaxis3 = list(fixedrange = TRUE),
+      yaxis4 = list(fixedrange = TRUE)
+    )
   
 }else{ # If only doing quality control on one video
   
-  # Set up behavior data frames - v1
-  tall_beh_v1 <- clean_raw_file(video1_info)$tall_data_beh
-  tall_mod_v1 <- clean_raw_file(video1_info)$tall_data_mod
+  video1_frames <- ready_to_plot(video1_info)
   
   # Total plot comparison (2 videos only)
   plot_beh <- plot_annotation(video1_info = video1_info,
-                              tall_data1 = tall_beh_v1)
-  
-  # Compare only periods with behaviors of interest
-  # Filter agreement data frames for only periods containing behaviors of interest
-  beh_int_frame1 <- tall_beh_v1 %>% filter(behavior %in% beh_of_interest)
-  
-  beh_int_frame1 <- add_other_rows(beh_int_frame1)
+                              tall_data1 = video1_frames$beh_frame)
   
   plot_beh_int <- plot_annotation(video1_info = video1_info,
-                                  tall_data1 = beh_int_frame1)
+                                  tall_data1 = video1_frames$beh_int_frame)
   
   ggplotly(plot_beh_int)
   ggplotly(plot_beh)
   
 } # End if else statement here
+
+# Shiny app ----
+
+# User interface
+ui <- fluidPage(
+  titlePanel("Interactive ggplotly Graph"),
+  mainPanel(
+    plotlyOutput("interactive_plot")
+  )
+)
+
+# Server
+server <- function(input, output) {
+  output$interactive_plot <- renderPlotly({
+    combined_plot
+  })
+}
+
+# Run the shiny app
+shinyApp(ui = ui, server = server)
